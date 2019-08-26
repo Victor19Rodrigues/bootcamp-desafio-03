fef94a7cc5ed8050926ac8a0ca5f86c37c2e9bc1
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-08-26 00:47:44 -0300

    + Created mail sender

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,8 @@ import pt from "date-fns/locale/pt";
 
 import Appointment from "../models/Appointment";
 import Notification from "../schemas/Notification";
+import Mail from "../../lib/Mail";
+
 import User from "../models/User";
 import File from "../models/File";
 
@@ -112,7 +114,15 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: "provider",
+                    attributes: ["name", "email"]
+                }
+            ]
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -131,6 +141,12 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: "Agendamento cancelado",
+            text: "Você tem um novo cancelamento"
+        });
 
         return res.json(appointment);
     }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "bf69fc4f023625",
+        pass: "ca84c77df42161"
+    },
+    default: {
+        from: "Equipe GoBarber <noreply@gobarber.com>"
+    }
+};
