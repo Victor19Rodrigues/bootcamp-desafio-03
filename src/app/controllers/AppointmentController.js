692b278c51b109b64e8678f915c1afb664dbd5c1
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-08-26 05:46:46 -0300

    + Configurated redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,8 +4,9 @@ import pt from "date-fns/locale/pt";
 
 import Appointment from "../models/Appointment";
 import Notification from "../schemas/Notification";
-import Mail from "../../lib/Mail";
+import Queue from "../../lib/Queue";
 
+import CancellationMail from "../jobs/CancellationMail";
 import User from "../models/User";
 import File from "../models/File";
 
@@ -147,19 +148,8 @@ class AppointmentController {
 
         await appointment.save();
 
-        await Mail.sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: "Agendamento cancelado",
-            template: "cancelation",
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    "'dia' dd 'de' MMMM', às' H:mm'h'",
-                    { locale: pt }
-                )
-            }
+        await Queue.add(CancellationMail.key, {
+            appointment
         });
 
         return res.json(appointment);
